bb449705b71ba3847161bb5a0a57a806840a4b8b
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-04 16:34:16 +0530

    Create individual.java

---
+++ individual.java
@@ -0,0 +1,34 @@
+public class individual {
+    public static void main(String ar[])throws Exception
+{
+int n,p,t;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int ar1[]=new int[n];
+for(int i=0;i<n;i++)
+ar1[i]=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+if(ar1[i]>ar1[j])
+{
+t=ar1[i];
+ar1[i]=ar1[j];
+ar1[j]=t;
+}
+}
+}
+for(int i=0;i<n-1;i++)
+{
+if(ar1[i]==ar1[i+1])
+{
+    i++;
+    continue;
+}
+else
+System.out.println(ar[i]);
+}
+
+}
+}
