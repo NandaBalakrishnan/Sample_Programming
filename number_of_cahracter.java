41cacda9a8901479eeaf3239c21a769d12a68ef3
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-05 16:23:12 +0530

    Create number_of_cahracter.java

---
+++ number_of_cahracter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class charArray {
+    public static void main(String ar[])throws Exception
+    {
+    Scanner sc=new Scanner(System.in);
+    String p=new String();
+    p=sc.next();
+    char c[]=p.toCharArray();
+    System.out.println(c.length);
+    }
+}
