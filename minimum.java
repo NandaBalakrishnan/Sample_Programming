472763349a2d8bd647be462a0b524cdaf117b0d0
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-05 13:23:13 +0530

    Create minimum.java
    first read the number as string
    convert into number and separate the integer into array
    we have a function called sort to sort the array and display the smallest number by eliminating until strips value
    finally the output will be obtained.

---
+++ minimum.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class minimum 
+{
+public static void main(String ar[])throws Exception
+{
+    int r,i,k;
+    int p[]=new int[100];
+Scanner sc=new Scanner(System.in);
+minimum m1=new minimum();
+String a=new String();
+a=sc.next();
+k=sc.nextInt();
+int n=Integer.parseInt(a);
+i=0;
+while(n>0)
+{
+r=n%10;
+p[i++]=r;
+n/=10;
+}
+m1.sort(p,k,i);
+}
+public void sort(int l[],int m,int g)
+{
+    int t;
+for(int i=0;i<g-1;i++)
+{
+for(int j=i+1;j<g;j++)
+{
+if(l[i]>l[j])
+{
+t=l[i];
+l[i]=l[j];
+l[j]=t;
+}
+}
+}
+for(int i=0;i<m;i++)
+    System.out.print(l[i]);
+}
+}
