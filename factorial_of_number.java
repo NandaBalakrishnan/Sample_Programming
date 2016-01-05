80649082dad046982f26bfb427076924b4cf6e0d
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-05 14:23:50 +0530

    Create factorial_of_number.java

---
+++ factorial_of_number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class factorial_of_number
+{
+public static void main(String ar[])throws Exception
+{
+int no;
+Scanner sc=new Scanner(System.in);
+no=sc.nextInt();
+for(int i=1;i<=no;i++)
+{
+f*=i;
+}
+System.out.println("factorial of given number is:"+f);
+}
+}
+
