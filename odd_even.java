e6179d942c1b31f7af18b7cbba4c1d5fb79d5507
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-05 14:09:32 +0530

    Create odd_even.java

---
+++ odd_even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class odd_even
+{
+public static void main(String ar[])throws Exception
+{
+int no;
+Scanner sc=new Scanner(System.in);
+no=sc.nextInt();
+if(no%2==0)
+System.out.println("Even number");
+else
+System.out.println("Odd number");
+}
+}
+
