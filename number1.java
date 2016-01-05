ff1d982f6bcae80db55df7662029daa45ad09618
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-05 14:03:50 +0530

    Create number1.java

---
+++ number1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class number1
+{
+public static void main(String ar[])
+{
+Scanner sc=new Scanner(System.in);
+int no;
+no=sc.nextInt();
+if(no>0)
+System.out.println("number is positive");
+else if(no==0)
+System.out.println("number is zero");
+else
+System.out.println("number is negative");
+}
+}
