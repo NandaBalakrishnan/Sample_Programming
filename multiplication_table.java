dfc718f17fdd3d9629c21c3e8bbbc9e748fdc147
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-05 14:22:58 +0530

    Create multiplication_table.java

---
+++ multiplication_table.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class multiplication_table
+{
+public static void main(String ar[])throws Exception
+{
+int no;
+Scanner sc=new Scanner(System.in);
+no=sc.nextInt();
+for(int i=1;i<=12;i++)
+{
+System.out.println(no+"*"+i+"="+no*i);
+}
+}
+}
+
