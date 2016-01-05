2772550b0b366a39e8957377f792ccd83e76335a
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-05 14:14:33 +0530

    Create sum_of_natural.java

---
+++ sum_of_natural.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sum_of_natural
+{
+public static void main(String ar[])throws Exception
+{
+int n,s=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+s=(n*(n+1))/2;
+System.out.println("sum of natural number is:"+s);
+}
+}
