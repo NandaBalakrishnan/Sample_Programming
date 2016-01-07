366b71d26afb2a5af99bb2cfec9df2dd4e3174f9
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-07 15:58:31 +0530

    Create even_number_intervals.java

---
+++ even_number_intervals.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class even_number_intervals
+{
+public static void main(String ar[])throws Exception
+{
+Scanner sc=new Scanner(System.in);
+int a,b;
+a=sc.nextInt();
+b=sc.nextInt();
+for(int i=a;i<=b;i++)
+{
+if(i%2==0)
+System.out.println(i);
+}
+}
+}
