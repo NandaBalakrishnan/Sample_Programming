046a2e2f8e5cc18e7c156a2b87f9be3063c98081
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-07 17:01:01 +0530

    Create multiplication_of_number.java

---
+++ multiplication_of_number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class multiplication_of_number
+{
+public static void main(String ar[])throws Exception
+{
+int no;
+Scanner sc=new Scanner(System.in);
+no=sc.nextInt();
+for(int i=1;i<=12;i++)
+{
+System.out.println(no+"*"+i+"="+no*i);
+}
+}
+}
