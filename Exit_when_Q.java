c31b4e4331b75f946198d9fc972ffbb705375e00
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-07 12:13:30 +0530

    Create Exit_when_Q.java

---
+++ Exit_when_Q.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Exit_when_Q
+{
+public static void main(String ar[])throws Exception
+{
+String p=new String();
+Scanner sc=new Scanner(System.in);
+p=sc.next();
+if(p.equals("Q"))
+System.exit(0);
+}
+}
