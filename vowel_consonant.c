87ed1fc9de26291d5b581d9f4865baefebfd5da9
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-08 17:08:08 +0530

    Create vowel_consonant.c

---
+++ vowel_consonant.c
@@ -0,0 +1,13 @@
+#inlcude<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+clrscr();
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'||)
+printf("vowel");
+else
+printf("consonant");
+getch();
+}
