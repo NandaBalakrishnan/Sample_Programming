0381d11bbc55c08bc66d2753aef88ce9e5501c9c
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-01-09 15:14:42 +0530

    Create valid_parenthesis.java

---
+++ valid_parenthesis.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class valid_parenthesis
+{
+public static void main(String ar[])throws Exception
+{
+Scanner sc=new Scanner(System.in);
+String p=new String();
+p=sc.next();
+int count=0;
+char c[]=p.toCharArray();
+for(int i=0;i<c.length;i++)
+{
+if(c[i]=='(')
+{
+if(c[i+1]==')')
+{
+count++;
+}
+}
+}
+System.out.println("count:"+count);
+}
+}
