d9d3b01dd11db2b8df570cea8d6c0b1ff8252b53
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-06-13 19:30:02 +0530

    Update distinct_elements.py

--- distinct_elements.py
+++ distinct_elements.py
@@ -1,7 +1,6 @@
 import itertools
 l=[str(j) for j in input().split()]
-s=str(''.join(l[0:len(l)]))
-print(s)
-t=list(itertools.permutations(s,2))
+t=list(itertools.permutations(l,2))
 m=[t[i] for i in range(len(t))  if(t[i][0] < t[i][1])]
-print("distinct elements",m)
+print(m)
+print("distinct elements",len(m))
