5035de339f839d25e874c236db8754fe1ac430dd
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-06-13 22:01:55 +0530

    Update queue_linklist.c

--- queue_linklist.c
+++ queue_linklist.c
@@ -1,99 +1,20 @@
 #include<stdio.h>
-#include<malloc.h>
-struct queue
-{
-int ele;
-struct queue*next;       
-}*front='\0',*rear='\0';
-void insert();
-void delete();
-void display();
+int length(char *);
 int main()
 {
-int ch;
-do
-{
-printf("1.insert\n2.delete\n3.display\n");
-printf("Enter the choice\n");
-scanf("%d",&ch);
-switch(ch)
-{
-case 1:
-     insert();
-     break;
-case 2:
-     delete();
-     break;
-case 3:
-     display();
-     break;                  
-}    
-}while(ch!=4);
-system("pause");
+int l;
+char *a=(char*)malloc(sizeof(char)*10);
+int(*fp)(char*)=length;
+printf("Enter the string\n");
+gets(a);
+l=(*fp)(a);
+printf("length:%d",l);
+system("pause");    
 }
-void insert()
+int length(char *p)
 {
- struct queue*newnode,*q;
- int x;
- newnode=(struct queue*)malloc(sizeof(struct queue));
- printf("Enter the element\n");
- scanf("%d",&x);
- newnode->ele=x;
- if(rear=='\0')
- {
- newnode->next='\0';
- front=newnode;
- rear=newnode;              
- }    
- else
- {
- q=front;
- while(q->next!='\0')
- {
- q=q->next;                    
- }
- q->next=newnode;
- newnode->next='\0';
- rear=newnode;
-     
- }
-}
-void delete()
-{
-     struct queue*q;
-     if(front=='\0')
-     {
-     printf("queue is empty\n");
-     }
-     else
-     {
-     q=front;    
-     if(front==rear)
-     {
-     front='\0';
-     rear='\0';               
-     }    
-     else
-     {
-      front=front->next;
-      printf("element is%d\t",q->ele);
-      free(q);   
-     }
-     }
-}
-void display()
-{
-     struct queue*q;
-
-     if(front=='\0')
-     printf("queue is empty");
-     else
-     {
-     q=front;
-     while(q!='\0')
-     {
-     printf("%d-->",q->ele);                    
-     q=q->next;
-     }    
-     }
+int i=0;
+while(p[i]!='\0')
+i++;
+return i;    
 }
