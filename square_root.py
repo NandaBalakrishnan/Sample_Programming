bb3edbd2b66c0d91c9f6dd054e35df4f1a5e00dc
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-06-13 12:16:43 +0530

    Create square_root.py

---
+++ square_root.py
@@ -0,0 +1,9 @@
+import math //import math function
+x1=int(input())
+x2=int(input())
+y1=int(input())
+y2=int(input())
+m=((x1+x2)*(x1+x2))//perform operation
+n=((y1+y2)*(y1+y2))
+ans=math.sqrt((m+n))//perform square root
+print("square root value:",int(ans))//take typecating of ans variable
