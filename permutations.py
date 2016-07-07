8feaf99270f0ffda7da65c2e78b283e8e5eb9663
NandaBalakrishnan <nandathoomathy@gmail.com>
2016-07-07 15:35:58 +0530

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,6 +1,8 @@
 import itertools
 s=str(input())
 s1=str(input())
-s2=(s+s1).strip("")
-t= ["".join(x) for x in itertools.permutations(s2,len(s2))]
-print(t)
+
+t=["".join(x) for x in itertools.permutations(s,len(s))]
+t1=["".join(y) for y in itertools.permutations(s1,len(s1))]
+for j in range(len(t)):
+    print(str(t[j])+str(t1[j]))
